707b19f9628dcc577a93a98b9c8b58f7807fb7da
Nikhil-Khetani <67691373+Nik-Khet@users.noreply.github.com>
2021-07-03 17:45:17 +0100

    Added some validity functions

--- game.py
+++ game.py
@@ -1,10 +1,34 @@
 import numpy as np
 
-class state:
-    def __init__(self, board=None):
+class State():
+    def __init__(self, board=None, rows = 8, cols = 8):
+        self.rows = rows
+        self.cols = cols
         if board == None:
-            self.board == np.zeros()
-        self.board = board
+            self.board = np.zeros((self.rows, self.cols))
+        else:
+            self.board = board
+        print(self.board)
+        print(self.validMoves())
+        pass
+
+    def validMoves(self):
+        valid_moves = []
+        for i in range(self.cols):
+            if self.board[0][i]==0:
+                valid_moves.append(i)
+        return valid_moves
+
+    def takeAction(self, action):
+        if action in self.validMoves():
+            new_board = self.board
+            
+            pass
+        else:
+            print("Invalid Action")
 
     def findChildren(self):
-        return {}
+        pass
+
+if __name__=="__main__":
+    game = State()
